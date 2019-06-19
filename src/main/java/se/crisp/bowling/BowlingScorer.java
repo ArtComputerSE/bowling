ca880fb9dd8fe2247aa97c533031f863345ea51d
lma <lma@ubuntulmalaptop.miclaser.net>
2019-06-19 14:34:47 +0200

    Added test

--- src/main/java/se/crisp/bowling/BowlingScorer.java
+++ src/main/java/se/crisp/bowling/BowlingScorer.java
@@ -3,6 +3,7 @@ package se.crisp.bowling;
 public class BowlingScorer {
 
     public int score(String pins) {
+    	char[] charArray = pins.toCharArray();
     	if ("00".equals(pins)) {
     		return 0;
     	}
