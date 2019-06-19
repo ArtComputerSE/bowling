3c169d403969bcdf4a052e1e893477476eef03b7
lma <lma@ubuntulmalaptop.miclaser.net>
2019-06-19 16:06:56 +0200

    Added more single frame tests

--- src/main/java/se/crisp/bowling/BowlingScorer.java
+++ src/main/java/se/crisp/bowling/BowlingScorer.java
@@ -1,20 +1,53 @@
 package se.crisp.bowling;
 
 public class BowlingScorer {
+	int INCOMPLETE_FRAME = 100;
 
-	public int score(String pins) {
+	public int fullScore(String pins) {
+		int sum = 0;
+		int mem = 0;
+
+		for (String frame : pins.split(" ")) {
+			int tmp = frameScore(frame);
+
+			if (tmp < 0) {
+				mem = tmp;
+			} else {
+				if (tmp != INCOMPLETE_FRAME) {
+					sum += tmp;
+					if (mem != 0) {
+						sum += -mem;
+						sum += tmp;
+					}
+				}
+			}
+		}
+		return sum;
+	}
+
+	private int frameScore(String pins) {
 		char[] charArray = pins.toCharArray();
 
-		int score = 0;
+		int sum = 0;
 		for (char c : charArray) {
-			if (c == '-') {
-				score += 0;
+			if (c == 'X') {
+				return -10;
+			} else if (charArray.length == 1) {
+				return INCOMPLETE_FRAME;
+			} else if (c == '/') {
+				if (charArray.length == 2) {
+					return 0;
+				} else {
+					sum = 10;
+				}
+			} else if (c == '-') {
+				sum += 0;
 			} else {
-				score += c - '0';
+				sum += c - '0';
 			}
 		}
 
-		return score;
+		return sum;
 	}
 
 }
--- src/test/java/se/crisp/bowling/BowlingScorerTest.java
+++ src/test/java/se/crisp/bowling/BowlingScorerTest.java
@@ -13,34 +13,59 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SuppressWarnings("WeakerAccess")
 public class BowlingScorerTest {
 
-    @Test
-    @DisplayName("All misses, no points")
-    public void when_frame_is_all_miss_then_no_points() {
-        BowlingScorer bowlingScorer = new BowlingScorer();
+	@Test
+	@DisplayName("All misses, no points")
+	public void when_frame_is_all_miss_then_no_points() {
+		BowlingScorer bowlingScorer = new BowlingScorer();
 
-        assertEquals(bowlingScorer.score("00"), 0);
-    }
-    
-    @Test
-    public void test_4_5 () {
-        BowlingScorer bowlingScorer = new BowlingScorer();
+		assertEquals(bowlingScorer.fullScore("00"), 0);
+	}
 
-        assertEquals(bowlingScorer.score("45"), 9);
-    }
+	@Test
+	public void test_4_5() {
+		BowlingScorer bowlingScorer = new BowlingScorer();
 
-    @ParameterizedTest
-    @DisplayName("Single frame")
-    @MethodSource("singleFrameCases")
-    public void single_frame(String pins, int expected) {
-        BowlingScorer bowlingScorer = new BowlingScorer();
+		assertEquals(bowlingScorer.fullScore("45"), 9);
+	}
 
-        assertEquals(expected, bowlingScorer.score(pins));
-    }
+	@Test
+	public void test_X_58() {
+		BowlingScorer bowlingScorer = new BowlingScorer();
 
-    private static Stream<Arguments> singleFrameCases(){
-        return Stream.of(
-                Arguments.arguments("-1", 1),
-                Arguments.arguments("1-", 1)
-        );
-    }
+		assertEquals(bowlingScorer.fullScore("X 58"), 10 + 5 + 8 + 5 + 8);
+	}
+
+	@Test
+	public void test_X_5() {
+		BowlingScorer bowlingScorer = new BowlingScorer();
+
+		assertEquals(bowlingScorer.fullScore("X 5"), 0);
+	}
+
+	@Test
+	public void test_3_spare_7() {
+		BowlingScorer bowlingScorer = new BowlingScorer();
+
+		assertEquals(bowlingScorer.fullScore("5/7"),10 + 7);
+	}
+
+	@Test
+	public void test_3_spare() {
+		BowlingScorer bowlingScorer = new BowlingScorer();
+
+		assertEquals(bowlingScorer.fullScore("5/"),0);
+	}
+
+	@ParameterizedTest
+	@DisplayName("Single frame")
+	@MethodSource("singleFrameCases")
+	public void single_frame(String pins, int expected) {
+		BowlingScorer bowlingScorer = new BowlingScorer();
+
+		assertEquals(expected, bowlingScorer.fullScore(pins));
+	}
+
+	private static Stream<Arguments> singleFrameCases() {
+		return Stream.of(Arguments.arguments("-1", 1), Arguments.arguments("1-", 1));
+	}
 }
