a439d55db00720cda72b593e99e0025e3cb28fd8
xabier1967 <xabier1967@hotmail.com>
2019-06-19 14:11:32 +0200

    Fixed existing tests

--- src/main/java/se/crisp/bowling/BowlingScorer.java
+++ src/main/java/se/crisp/bowling/BowlingScorer.java
@@ -3,7 +3,18 @@ package se.crisp.bowling;
 public class BowlingScorer {
 
     public int score(String pins) {
-        return 0;
+        int len = pins.length();
+        int sum = 0;
+        for (int i = 0; i <  len; i++) {
+
+            if(pins.charAt(i) == '-') {
+                sum += 0;
+            }
+            else {
+                sum += Character.getNumericValue(pins.charAt(i));
+            }
+        }
+        return sum;
     }
 
 }
