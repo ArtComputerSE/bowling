c8428360992065058ef447ddfce6816d11243bb6
stefan fu <stefan.fu@mycronic.com>
2019-06-19 15:03:02 +0200

    Single frame.

--- src/main/java/se/crisp/bowling/BowlingScorer.java
+++ src/main/java/se/crisp/bowling/BowlingScorer.java
@@ -12,13 +12,20 @@ public class BowlingScorer {
             System.out.println(pins);
             if (pins.charAt(0) == 'X') {
                 pins = pins.substring(1);
+                if (pins.length() >= 2) {
+                    if (pins.charAt(1) == '/') {
+                        sum += 20;
+                    } else {
+                        sum += (rawScore(pins.substring(0, 2)) + 10);
+                    }
+                }
             } else {
                 if (pins.length() == 1) {
                     sum = sum + Character.getNumericValue(pins.charAt(0));
                 } else if (pins.charAt(1) == '/') {
                     ;
                 } else {
-                    sum = sum + Character.getNumericValue(pins.charAt(0)) + Character.getNumericValue(pins.charAt(1));
+                    sum += rawScore(pins.substring(0, 2));
                 }
 
                 pins = pins.substring(2);
@@ -29,6 +36,20 @@ public class BowlingScorer {
 
 
         return sum;
+    }
+
+    private int rawScore(String subString) {
+        int total = 0;
+
+        for (int i = 0; i < subString.length(); i++) {
+            if( Character.isDigit(subString.charAt(i)) ){
+                total = total + Character.getNumericValue(subString.charAt(i));
+            } else if (subString.charAt(i) == 'X') {
+                total += 10;
+            }
+        }
+        return total;
+
     }
 
 }
