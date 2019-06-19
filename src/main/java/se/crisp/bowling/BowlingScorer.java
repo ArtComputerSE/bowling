5322e28a278a158563d6024d509a5d055f10e707
Erik Norhammar <erik.norhammar@mycronic.com>
2019-06-19 16:40:12 +0200

    Added support for partial games with just digits, specifically "45"

--- src/main/java/se/crisp/bowling/BowlingScorer.java
+++ src/main/java/se/crisp/bowling/BowlingScorer.java
@@ -12,6 +12,13 @@ class BowlingScorer {
 
         for (String ch : characters) {
             if (ch.equals("1")) score += 1;
+            if (ch.equals("2")) score += 2;
+            if (ch.equals("3")) score += 3;
+            if (ch.equals("4")) score += 4;
+            if (ch.equals("5")) score += 5;
+            if (ch.equals("6")) score += 6;
+            if (ch.equals("7")) score += 7;
+            if (ch.equals("8")) score += 8;
             if (ch.equals("9")) score += 9;
         }
 
