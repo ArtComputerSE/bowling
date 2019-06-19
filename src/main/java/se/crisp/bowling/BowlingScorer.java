676cdc4eab0de503f44ada4b287127315d2481bf
juan <juan.albahaca@mycronic.se>
2019-06-19 14:16:36 +0200

    initial tests pass, implemented constructor and chartoint map.

--- src/main/java/se/crisp/bowling/BowlingScorer.java
+++ src/main/java/se/crisp/bowling/BowlingScorer.java
@@ -1,9 +1,35 @@
 package se.crisp.bowling;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BowlingScorer {
 
+    public BowlingScorer() {
+        m.put('1', 1);
+        m.put('-', 0);
+        m.put('0', 0);
+    }
+
+    private Map<Character, Integer> m = new HashMap<Character, Integer>();
+
+    private String[] scoreChars;
+
+    private int sumFrame(char[] frame) {
+        int sum = 0;
+        for (char c:frame) {
+            sum += m.get(c);
+        }
+        return sum;
+    }
+
     public int score(String pins) {
-        return 0;
+        char[] splitFrame = pins.toCharArray();
+        return sumFrame(splitFrame);
     }
 
 }
+
+//String[] splitPins = str.split(" ");
