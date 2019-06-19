d34146cfc91860be26a00e30a225dab09a5d9016
Flore <flore.remouit@mycronic.com>
2019-06-19 14:47:25 +0200

    Version 1 bowling

--- src/main/java/se/crisp/bowling/BowlingScorer.java
+++ src/main/java/se/crisp/bowling/BowlingScorer.java
@@ -1,9 +1,36 @@
 package se.crisp.bowling;
 
+import java.util.Objects;
+
 public class BowlingScorer {
 
     public int score(String pins) {
-        return 0;
+
+        int score = 0;
+        int i=0;
+        while(i<pins.length()) {
+            char c = pins.charAt(i);
+            if (Character.isDigit(c)) {
+                score += Character.getNumericValue(c);
+            }
+            else if (Objects.equals(c, '-')) {
+                score += 0;
+            }
+            else if (Objects.equals(c, 'X')) {
+                if (i>=pins.length()-4) {
+                    score += 0;
+                    break;
+                }
+            }
+            else if (Objects.equals(c, '/')) {
+                if (i>=pins.length()-3) {
+                    score -= Character.getNumericValue(pins.charAt(i-1));
+                    break;
+                }
+            }
+            i++;
+        }
+        return score;
     }
 
 }
--- src/test/java/se/crisp/bowling/BowlingScorerTest.java
+++ src/test/java/se/crisp/bowling/BowlingScorerTest.java
@@ -33,7 +33,26 @@ public class BowlingScorerTest {
     private static Stream<Arguments> singleFrameCases(){
         return Stream.of(
                 Arguments.arguments("-1", 1),
-                Arguments.arguments("1-", 1)
+                Arguments.arguments("1-", 1),
+                Arguments.arguments("23", 5),
+                Arguments.arguments("2/", 0)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Double frame")
+    @MethodSource("doubleFrameCases")
+    public void double_frame(String pins, int expected) {
+        BowlingScorer bowlingScorer = new BowlingScorer();
+
+        assertEquals(expected, bowlingScorer.score(pins));
+    }
+
+    private static Stream<Arguments> doubleFrameCases(){
+        return Stream.of(
+                Arguments.arguments("-1 34", 8),
+                Arguments.arguments("1- X", 1),
+                Arguments.arguments("X 3", 0)
         );
     }
 }
