91e3fd45e474a3c7b74f24d348add47dffa7a36f
Erik Norhammar <erik.norhammar@mycronic.com>
2019-08-30 17:13:51 +0200

    Added checking for illegal characters

--- python/bowling/bowling_scorer.py
+++ python/bowling/bowling_scorer.py
@@ -1,3 +1,18 @@
 class BowlingScorer:
-    def score(self, pins):
+    def score(self, record):
+        check_for_illegal_characters(record)
         return 0
+
+
+class IllegalCharacter(ValueError):
+    pass
+
+
+legalDigits = "123456789"
+legalCharacters = legalDigits + " -/X"
+
+
+def check_for_illegal_characters(record):
+    for character in record:
+        if character not in legalCharacters:
+            raise IllegalCharacter(character)
--- python/tests/bowling/test_bowling_scrorer.py
+++ python/tests/bowling/test_bowling_scrorer.py
@@ -1,6 +1,23 @@
-from bowling.bowling_scorer import BowlingScorer
+import pytest
+
+from bowling.bowling_scorer import BowlingScorer, IllegalCharacter
 
 
 def test_no_score():
     scorer = BowlingScorer()
     assert scorer.score("--") == 0
+
+
+def test_all_misses():
+    scorer = BowlingScorer()
+    assert scorer.score("--------------------") == 0
+
+
+@pytest.fixture(params=['a', 'ö', ',', '&', '|', '\\'])
+def illegal_character(request):
+    return request.param
+
+
+def test_illegal_characters(illegal_character):
+    with pytest.raises(IllegalCharacter):
+        BowlingScorer().score(illegal_character)
